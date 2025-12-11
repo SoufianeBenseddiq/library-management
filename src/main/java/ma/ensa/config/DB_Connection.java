ef2dd010eb24cc7879c9ceebfee47603d6a58f71
Soufiane Benseddiq <soufianebenseddiq8@gmail.com>
2025-12-11 13:02:27 +0100

    init project

---
+++ src/main/java/ma/ensa/config/DB_Connection.java
@@ -0,0 +1,12 @@
+package ma.ensa.config;
+
+import java.sql.*;
+
+public class DB_Connection {
+    private Connection con;
+    private Statement st;
+
+    public DB_Connection() {
+        Class.forName("")
+    }
+}
